f232cc72a8fe3c3ca2caeb0a7480f016195f4adb
DCTewi <dctewi@dctewi.com>
2019-02-02 12:03:05 +0800

    Particle Sys Laerning

---
+++ Scripts/My Particles/ParticleFire.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleFire : MonoBehaviour
+{
+    public List<ParticleHelpper> particles;
+
+    private void Update()
+    {
+        foreach (ParticleHelpper ph in particles)
+        {
+            if (ph.varyAlpha)
+            {
+                ph.IncreaseAlpha();
+            }
+            //...
+        }
+    }
+}
---
+++ Scripts/My Particles/ParticleHelpper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleHelpper : MonoBehaviour
+{
+    public ParticleSystem.MainModule part;
+    public bool varyAlpha;
+    public float minAlpha;
+    public float maxAlpha;
+    public float alphaIncreaseRate;
+    public float alphaDecreaseRate;
+    public float alphaVariation;
+
+    public void IncreaseAlpha()
+    {
+        if (part.startColor.color.a < maxAlpha)
+        {
+            Color adjColor = part.startColor.color;
+            adjColor.a += alphaIncreaseRate * Time.deltaTime;
+            part.startColor = adjColor;
+        }
+    }
+
+    public void DecreaseAlpha()
+    {
+        if (part.startColor.color.a > minAlpha)
+        {
+            Color adjColor = part.startColor.color;
+            adjColor.a -= alphaDecreaseRate * Time.deltaTime;
+            part.startColor = adjColor;
+        }
+    }
+
+    //etc.
+	
+}
